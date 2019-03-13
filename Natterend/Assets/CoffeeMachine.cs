cc52a7ffe42bbdef8a60d370464c5e2826b69d8e
Cyandog <kbo0506@gmail.com>
2019-03-13 20:05:30 +0100

    Added Coffee Machine

---
+++ Natterend/Assets/CoffeeMachine.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoffeeMachine : MonoBehaviour
+{
+    public GameObject player;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        RefreshCoffee();
+    }
+
+    void RefreshCoffee()
+    {      
+        player.GetComponent<CoffeeController>().RefreshCoffee();
+    }
+}
